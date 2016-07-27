332b2fd99b75b51d5b61dd9ba9bbd06b2735e56e
nick Hill <nick-9979@outlook.com>
2016-07-27 14:03:02 +0100

    First real work.  Create the Virtual Network

---
+++ CitrixCloudInfrastructure/Scripts/Start-AzureRMRGVMs.ps1
@@ -0,0 +1,19 @@
+﻿
+get-azurermsubscription
+Select-AzureRmSubscription -SubscriptionId 796b222b-b926-4bd5-a6c6-b7f6041433d7
+$RG = Get-AzureRMResourceGroup -name "cc-rg"
+$VMs = Get-AzureRmVM -ResourceGroupName $RG.ResourceGroupName
+                                  foreach($VM in $VMs)
+                                  {
+                                      $VMDetail = Get-AzureRmVM -ResourceGroupName $RG.ResourceGroupName -Name $VM.Name -Status
+                                      foreach ($VMStatus in $VMDetail.Statuses)
+                                      { 
+                                          if($VMStatus.Code.CompareTo("PowerState/deallocated") -eq 0)
+                                          {
+                                              $VMStatusDetail = $VMStatus.DisplayStatus
+                                              
+                                          }
+                                      }
+                                      start-azurermvm -name $vm.Name -ResourceGroupName $rg.ResourceGroupName
+                                      write-output $VM.Name " started"
+                                  }
